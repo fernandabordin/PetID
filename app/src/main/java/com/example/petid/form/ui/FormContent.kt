5ba5ba060eb76f40113fd1b2884bde8f987be111
Fernanda Bordin <fer_aisha@hotmail.com>
2024-10-30 00:04:34 -0300

    alteração de cor e arredondamento das bordas alteração de cor do botão, labels e caixa de texto criado o efeito de remover foco das caixas de texto e removido efeito de click ao clicar fora

--- app/src/main/java/com/example/petid/form/ui/FormContent.kt
+++ app/src/main/java/com/example/petid/form/ui/FormContent.kt
@@ -1,41 +1,57 @@
 package com.example.petid.form.ui
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun FormContent() {
+    val focusManager = LocalFocusManager.current
+    val interactionSource = remember { MutableInteractionSource() }
     Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp)
             .background(color = Color.White)
+            .clickable(interactionSource = interactionSource, indication = null) {
+                focusManager.clearFocus()
+            }
     ) {
+
         val name = remember { mutableStateOf("")}
         val age = remember { mutableStateOf("")}
         val color = remember { mutableStateOf("")}
+        val cutePink = Color(0xFFF2668B)
+        val containerColor = Color(0xFFF2F2F2)
 
         OutlinedTextField(
             modifier = Modifier.fillMaxWidth(),
             colors = OutlinedTextFieldDefaults.colors(
-                unfocusedBorderColor = Color.Magenta
+                focusedContainerColor = containerColor,
+                unfocusedBorderColor = cutePink,
+                focusedBorderColor = cutePink,
+                focusedLabelColor = cutePink,
+                unfocusedLabelColor = cutePink
             ),
+            shape = RoundedCornerShape(18.dp),
             value = name.value,
             label = {
                     Text(text = "Nome")
@@ -48,6 +64,14 @@ fun FormContent() {
 
         OutlinedTextField(
             modifier = Modifier.fillMaxWidth(),
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedContainerColor = containerColor,
+                unfocusedBorderColor = cutePink,
+                focusedBorderColor = cutePink,
+                focusedLabelColor = cutePink,
+                unfocusedLabelColor = cutePink
+            ),
+            shape = RoundedCornerShape(18.dp),
             value = age.value,
             label = {
                 Text(text = "Idade")
@@ -60,6 +84,14 @@ fun FormContent() {
 
         OutlinedTextField(
             modifier = Modifier.fillMaxWidth(),
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedContainerColor = containerColor,
+                unfocusedBorderColor = cutePink,
+                focusedBorderColor = cutePink,
+                focusedLabelColor = cutePink,
+                unfocusedLabelColor = cutePink
+            ),
+            shape = RoundedCornerShape(18.dp),
             value = color.value,
             label = {
                 Text(text = "Cor")
@@ -73,7 +105,7 @@ fun FormContent() {
         Button(
             modifier = Modifier.fillMaxWidth(),
             colors = ButtonDefaults.buttonColors(
-                containerColor = Color.Magenta
+                containerColor = cutePink
             ),
             onClick = {},
             content = {
