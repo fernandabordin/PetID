1e4e692ee8ea4a70aec7e237fb28c58d908cda18
Fernanda <guilherme.lima@brmalls.com.br>
2025-06-09 20:17:33 -0300

    Adicionado mais um campo de texto e ajuste de botao

--- app/src/main/java/com/example/petid/form/ui/FormContent.kt
+++ app/src/main/java/com/example/petid/form/ui/FormContent.kt
@@ -3,6 +3,8 @@ package com.example.petid.form.ui
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -16,6 +18,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
@@ -26,88 +29,113 @@ import androidx.compose.ui.unit.dp
 fun FormContent() {
     val focusManager = LocalFocusManager.current
     val interactionSource = remember { MutableInteractionSource() }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp)
+            .padding(all = 16.dp)
             .background(color = Color.White)
             .clickable(interactionSource = interactionSource, indication = null) {
                 focusManager.clearFocus()
-            }
-    ) {
+            },
+        verticalArrangement = Arrangement.SpaceBetween
 
-        val name = remember { mutableStateOf("")}
-        val age = remember { mutableStateOf("")}
-        val color = remember { mutableStateOf("")}
+    ) {
+        val name = remember { mutableStateOf("") }
+        val age = remember { mutableStateOf("") }
+        val color = remember { mutableStateOf("") }
         val cutePink = Color(0xFFF2668B)
         val containerColor = Color(0xFFF2F2F2)
 
-        OutlinedTextField(
-            modifier = Modifier.fillMaxWidth(),
-            colors = OutlinedTextFieldDefaults.colors(
-                focusedContainerColor = containerColor,
-                unfocusedBorderColor = cutePink,
-                focusedBorderColor = cutePink,
-                focusedLabelColor = cutePink,
-                unfocusedLabelColor = cutePink
-            ),
-            shape = RoundedCornerShape(18.dp),
-            value = name.value,
-            label = {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedContainerColor = containerColor,
+                    unfocusedBorderColor = cutePink,
+                    focusedBorderColor = cutePink,
+                    focusedLabelColor = cutePink,
+                    unfocusedLabelColor = cutePink
+                ),
+                shape = RoundedCornerShape(18.dp),
+                value = name.value,
+                label = {
                     Text(text = "Nome")
-            },
-            onValueChange = {newValue ->
-                name.value = newValue
+                },
+                onValueChange = { newValue ->
+                    name.value = newValue
 
-            }
-        )
+                }
+            )
 
-        OutlinedTextField(
-            modifier = Modifier.fillMaxWidth(),
-            colors = OutlinedTextFieldDefaults.colors(
-                focusedContainerColor = containerColor,
-                unfocusedBorderColor = cutePink,
-                focusedBorderColor = cutePink,
-                focusedLabelColor = cutePink,
-                unfocusedLabelColor = cutePink
-            ),
-            shape = RoundedCornerShape(18.dp),
-            value = age.value,
-            label = {
-                Text(text = "Idade")
-            },
-            onValueChange = {newValue ->
-                age.value = newValue
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedContainerColor = containerColor,
+                    unfocusedBorderColor = cutePink,
+                    focusedBorderColor = cutePink,
+                    focusedLabelColor = cutePink,
+                    unfocusedLabelColor = cutePink
+                ),
+                shape = RoundedCornerShape(18.dp),
+                value = age.value,
+                label = {
+                    Text(text = "Idade")
+                },
+                onValueChange = { newValue ->
+                    age.value = newValue
 
-            }
-        )
+                }
+            )
 
-        OutlinedTextField(
-            modifier = Modifier.fillMaxWidth(),
-            colors = OutlinedTextFieldDefaults.colors(
-                focusedContainerColor = containerColor,
-                unfocusedBorderColor = cutePink,
-                focusedBorderColor = cutePink,
-                focusedLabelColor = cutePink,
-                unfocusedLabelColor = cutePink
-            ),
-            shape = RoundedCornerShape(18.dp),
-            value = color.value,
-            label = {
-                Text(text = "Cor")
-            },
-            onValueChange = {newValue ->
-                color.value = newValue
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedContainerColor = containerColor,
+                    unfocusedBorderColor = cutePink,
+                    focusedBorderColor = cutePink,
+                    focusedLabelColor = cutePink,
+                    unfocusedLabelColor = cutePink
+                ),
+                shape = RoundedCornerShape(18.dp),
+                value = color.value,
+                label = {
+                    Text(text = "Cor")
+                },
+                onValueChange = { newValue ->
+                    color.value = newValue
 
-            }
-        )
+                }
+            )
 
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedContainerColor = containerColor,
+                    unfocusedBorderColor = cutePink,
+                    focusedBorderColor = cutePink,
+                    focusedLabelColor = cutePink,
+                    unfocusedLabelColor = cutePink
+                ),
+                shape = RoundedCornerShape(18.dp),
+                value = color.value,
+                label = {
+                    Text(text = "Espécie")
+                },
+                onValueChange = { newValue ->
+                    color.value = newValue
+
+                }
+            )
+        }
         Button(
-            modifier = Modifier.fillMaxWidth(),
-            colors = ButtonDefaults.buttonColors(
-                containerColor = cutePink
-            ),
             onClick = {},
+            modifier = Modifier
+                .fillMaxWidth(),
+            colors = ButtonDefaults.buttonColors(containerColor = cutePink),
             content = {
                 Text(text = "Confirmar")
             }
